c8340316eff476b9be78ea4c21d651d43e06c241
Clearly <46466387+Clearlyio@users.noreply.github.com>
2020-10-04 20:28:19 -0700

    InitalCommit

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,70 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+
   </div>
 </template>
 
-<script>
-import HelloWorld from "./components/HelloWorld.vue";
+<style lang="stylus" src="./assets/style.styl"></style>
 
+<script>
 export default {
-  name: "App",
-  components: {
-    HelloWorld
+  data: () => ({
+    allHandlers: [],
+    source: "",
+    timeout: 30000,
+    title: "",
+    body: "",
+    combined: ""
+  }),
+  methods: {},
+
+  mounted() {
+    const tmi = require("tmi.js");
+    const client = new tmi.Client({
+      connection: {
+        secure: true,
+        reconnect: true
+      },
+      
+      channels: ["thiswaifu"]
+    });
+
+    client.connect();
+
+    client.on("message", (channel, tags, message) => {
+      this.source = `${tags["display-name"]} : \n ${message}`;
+      this.title = `${tags["display-name"]}`;
+      this.body = `${message}`;
+      this.combined = '[ "' + this.title.toUpperCase() + '" ]' + " : \n" + ">  " + this.body;
+
+      // TODO find a way to get twitch user img
+      this.$toast(this.combined, {
+        position: "bottom-right",
+        timeout: this.timeout,
+        closeOnClick: false,
+        pauseOnFocusLoss: false,
+        pauseOnHover: false,
+        draggable: false,
+        draggablePercent: 0.1,
+        showCloseButtonOnHover: false,
+        hideProgressBar: false,
+        closeButton: false,
+        icon: false,
+        toastClassName: "my-custom-toast-class",
+        bodyClassName: ["custom-class-1", "custom-class-2"],
+        transition: "Vue-Toastification__fade",
+        maxToasts: 10,
+        rtl: false
+      });
+     /* this.$toast(this.combined, {
+        timeout: this.timeout,
+        position: POSITION.BOTTOM_RIGHT,  
+        toastClassName: "my-custom-toast-class",
+        bodyClassName: ["custom-class-1", "custom-class-2"],
+        transition: "Vue-Toastification__fade",
+        maxToasts: 10
+      });*/
+    });
   }
 };
 </script>
-
-<style lang="stylus">
-#app
-  font-family Avenir, Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
-  margin-top 60px
-</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 import Vue from "vue";
 import App from "./App.vue";
+import vuetify from "./plugins/vuetify";
+import Toast from "vue-toastification";
+// Import the CSS or use your own!
+import "vue-toastification/dist/index.css";
 
 Vue.config.productionTip = false;
-
+Vue.use(Toast, {
+  transition: "Vue-Toastification__fade",
+  maxToasts: 10,
+  newestOnTop: true
+});
 new Vue({
+  vuetify,
   render: h => h(App)
 }).$mount("#app");
